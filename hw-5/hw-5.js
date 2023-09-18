07489b3b52eb6ca0fee59e39fd6195912458fa55
Viktor Pirogov <Blade-85@inbox.ru>
2023-09-18 23:11:04 +0300

    2.5 Начало домашки

---
+++ hw-5/hw-5.js
@@ -0,0 +1,89 @@
+// Задание 1
+// function difference(a, b){ 
+//     if ((a < b) || (a===b)) {
+//       return a ;  
+//     }
+//     else{
+//       return b;
+//     }
+// }
+// console.log(difference(8, 4));
+// Задание 2
+// function evenNumber(n) {
+//    if(n % 2===0){
+//      console.log(`Число четное`);
+//    }
+//    else {
+//      console.log(`Число нечетное`);
+//    }
+// }
+// console.log(evenNumber(21));
+// Задание 3
+// const degreeNumber = a =>  {
+//     return a**2;
+// }
+// console.log(degreeNumber(9));
+// Задание 4
+// function ageUser(a) {
+//     if (a < 0) {
+//        console.log(`Вы ввели неправильное значение`) ;
+//     }
+//     if ((a <= 12) && (a > 0) ){
+//         console.log(`Привет, друг!`);
+//     }
+//     else {
+//         console.log(`Добро пожаловать!`);
+//     }
+// }
+//   console.log(ageUser(12));
+// Задание 5
+// function dataUser(a, b) {
+//     if ((isNaN(a) ) || (isNaN(b))) {
+//       console.log(`Одно или оба значения не являются числом`) ; 
+//     }
+//     else{
+//     return a * b;
+//     }
+// }
+    //  console.log(dataUser(7, 5));
+// Задание 6
+// function numberUser(n) {
+//     if (isNaN(n)) {
+//        console.log(`Переданный параметр не является числом`);
+//     }
+//     else {
+//         return n**3;
+//     }
+// }
+// console.log (numberUser(7));
+// Задание 7
+// function getRectangleArea() {
+//     return this.radius ** 2 * 3.14;
+// }
+// function getRectanglePerimiter() {
+//    return this.radius * 2 * 3.14; 
+// }
+// const circle1={ 
+//     radius:5,
+
+//     getArea: getRectangleArea,
+//     getPerimiter: getRectanglePerimiter,
+// }
+
+// const circle2={ 
+//     radius:7,
+
+//     getArea: getRectangleArea,
+//     getPerimiter: getRectanglePerimiter,
+// }
+//     console.log(circle1.getArea());
+//     console.log(circle1.getPerimiter());
+//     console.log(circle2.getArea());
+//     console.log(circle2.getPerimiter());
+
+
+
+    
+
+
+
