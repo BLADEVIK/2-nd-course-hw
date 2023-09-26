558cf420d27c87c095abe38e0dcae298b1c5f4e2
Viktor Pirogov <Blade-85@inbox.ru>
2023-09-26 22:50:51 +0300

    2.7 Начало домашки

---
+++ hw-7/hw-7.js
@@ -0,0 +1,56 @@
+// Задание 1
+// console.log(`Hello World`.toUpperCase());
+
+// Задание 2
+// const array = ['Кошка', 'Кит', 'Комар', 'Носорог'];
+// const search = `ко`
+// array.forEach(element => {
+//     if (element.toLowerCase().startsWith(search.toLowerCase())) {
+// 		console.log(element);
+// 	} 
+// });
+
+// Задание 3
+// console.log(Math.floor(32.58884));
+// console.log(Math.ceil(32.58884));
+// console.log(Math.round(32.58884));
+
+// Задание 4
+// console.log(Math.max(52, 53, 49, 77, 21, 32));
+// console.log(Math.min(52, 53, 49, 77, 21, 32));
+
+// Задание 5
+// function getRandomNumber(min,max) {
+//    return Math.floor(Math.random() * (max - min + 1)) + min 
+// }
+// console.log(getRandomNumber(1, 10));
+
+// Задание 6
+// const randomInt = (min, max) => { 
+//     let randomUser = min + Math.random() * (max + 1 - min); 
+//      return Math.floor(randomUser); 
+//    } 
+    
+//    const getRandomArrNumbers = (userNumber) => { 
+//     let array = []; 
+//     for (let i = 0; array.length < Math.floor((userNumber / 2)); i++) { 
+//      array.push(randomInt(0, userNumber)); 
+//     } 
+//     console.log(array); 
+//    } 
+    
+//    getRandomArrNumbers(7);
+
+// Задание 7
+// const randomInt = (min, max) => { 
+//          let randomUser = min + Math.random() * (max + 1 - min); 
+//           return Math.floor(randomUser); 
+// }
+// console.log(randomInt(2,8));
+
+// Задание 8
+// let currentDate = new Date();
+// console.log(currentDate);
+
+// Задание 9
+
